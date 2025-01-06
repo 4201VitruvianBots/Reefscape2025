e5deec0a17fb501b3aa600ce3fd8e0a0522549dc
Jonathan Dao <jonathandao0@gmail.com>
2025-01-05 18:04:36 -0800

    Merge 2024 Drivetrain Code
    Merge of 2024 Drivetrain code with 2025 Phoenix 6 library changes.
    TODO: Verify all swerve constants

---
+++ src/main/java/frc/robot/constants/CAN.java
@@ -0,0 +1,22 @@
+package frc.robot.constants;
+
+public class CAN {
+  public static final String rioCanbus = "rio";
+  public static String driveBaseCanbus = "drivebase";
+
+  public static final int pigeon = 9;
+
+  public static final int frontLeftCanCoder = 10;
+  public static final int frontRightCanCoder = 11;
+  public static final int backLeftCanCoder = 12;
+  public static final int backRightCanCoder = 13;
+
+  public static final int frontLeftDriveMotor = 20;
+  public static final int frontLeftTurnMotor = 21;
+  public static final int frontRightDriveMotor = 22;
+  public static final int frontRightTurnMotor = 23;
+  public static final int backLeftDriveMotor = 24;
+  public static final int backLeftTurnMotor = 25;
+  public static final int backRightDriveMotor = 26;
+  public static final int backRightTurnMotor = 27;
+}
