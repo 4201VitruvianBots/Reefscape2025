4e0fb38f37fc088560120f4dc542c89ad258ca69
Ronan-B <rdavidbouza@gmail.com>
2025-01-18 14:16:20 -0800

    More complicated auto

--- src/main/java/frc/robot/commands/TestAuto1.java
+++ src/main/java/frc/robot/commands/TestAuto1.java
@@ -4,16 +4,43 @@
 
 package frc.robot.commands;
 
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class TestAuto1 extends SequentialCommandGroup {
-  /** Creates a new TestAuto1. */
-  public TestAuto1() {
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+  /** Creates a new DriveForward. */
+  public TestAuto1(CommandSwerveDrivetrain swerveDrive /* TODO: Add field sim */) {
+    try {
+      PathPlannerPath path = PathPlannerPath.fromPathFile("Test1");
+
+      var m_ppCommand = AutoBuilder.followPath(path);
+
+      var point = new SwerveRequest.PointWheelsAt();
+      var stopRequest = new SwerveRequest.ApplyRobotSpeeds();
+
+      // Will throw an exception if the starting pose is not present
+      var starting_pose = path.getStartingHolonomicPose().orElseThrow();
+
+      // Add your commands in the addCommands() call, e.g.
+      // addCommands(new FooCommand(), new BarCommand());
+      addCommands(
+          new InstantCommand( // Reset the pose of the robot to the starting pose of the path
+              () -> swerveDrive.resetPose(starting_pose)),
+          new InstantCommand(
+                  () -> swerveDrive.applyRequest(() -> point.withModuleDirection(new Rotation2d())),
+                  swerveDrive)
+              .alongWith(new WaitCommand(1)),
+          m_ppCommand.andThen(() -> swerveDrive.setControl(stopRequest)));
+    } catch (Exception e) {
+      DriverStation.reportError("Failed to load path for DriveForward", e.getStackTrace());
+      addCommands(new WaitCommand(0));
+    }
   }
 }
