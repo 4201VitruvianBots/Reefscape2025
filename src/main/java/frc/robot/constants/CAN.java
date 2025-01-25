f5e2351bc2c55eef363446afcc69d1121197cdc1
Gavin P <gavinjpeeler@gmail.com>
2025-01-24 17:29:45 -0800

    Defined CAN ids

--- src/main/java/frc/robot/constants/CAN.java
+++ src/main/java/frc/robot/constants/CAN.java
@@ -20,6 +20,25 @@ public class CAN {
   public static final int backRightDriveMotor = 26;
   public static final int backRightTurnMotor = 27;
 
-  public static final int coralOuttakeMotor = 30;
-  public static final int algaeMotor = 31;
+  // L2-L4
+  public static final int hopperIntakeMotor1 = 30;
+  public static final int hopperIntakeMotor2 = 31;
+  public static final int outtakePivotMotor = 32;
+  public static final int elevatorMotor1 = 33;
+  public static final int elevatorMotor2 = 34;
+  public static final int endEffectorMotor = 35;
+  
+  public static final int outtakePivotCANcoder = 36;
+  
+  // L1/Ground
+  public static final int groundRollerMotor = 40;
+  public static final int groundDropdownMotor = 41;
+  
+  public static final int groundDropdownCANcoder = 42;
+  
+  // Climber
+  public static final int cageGrabberMotor = 50;
+  public static final int climberMotor = 51;
+  
+  public static final int climberCANcoder = 52;
 }
