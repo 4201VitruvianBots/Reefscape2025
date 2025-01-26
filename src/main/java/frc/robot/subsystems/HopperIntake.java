ac66b4902f79dde65478404ea911c718b29d40e6
DeNeefe <puzzlekitticatty@gmail.com>
2025-01-26 15:40:06 -0800

    created talonfx motors

--- src/main/java/frc/robot/subsystems/HopperIntake.java
+++ src/main/java/frc/robot/subsystems/HopperIntake.java
@@ -1,17 +1,17 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.V2CAN;
 
 public class HopperIntake extends SubsystemBase {
-  /** Creates a new HopperIntake. */
+
+  private final TalonFX m_intakeMotors[] = {new TalonFX(V2CAN.hopperIntakeMotor1), new TalonFX(V2CAN.hopperIntakeMotor2)};
+
   public HopperIntake() {}
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
   }
 }
