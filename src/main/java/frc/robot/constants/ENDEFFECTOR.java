8c73218aa85464049035d983a8bc1ada6b2a7fcd
Ronan-B <114883633+Ronan-B@users.noreply.github.com>
2025-01-26 23:43:07 +0000

    Automatically applied spotless

--- src/main/java/frc/robot/constants/ENDEFFECTOR.java
+++ src/main/java/frc/robot/constants/ENDEFFECTOR.java
@@ -6,14 +6,12 @@ public class ENDEFFECTOR {
   public static final double kD = 0.0;
   public static final double endEffectorGearRatio = 1.0;
 
-  //Pivot motor stuff
+  // Pivot motor stuff
   public static final double kPivotP = 0.0;
   public static final double kPivotI = 0.0;
   public static final double kPivotD = 0.0;
 
   public static final double endEffectorPivotGearRatio = 1.0;
 
-  public enum STATE {
-    
-  }
+  public enum STATE {}
 }
--- src/main/java/frc/robot/subsystems/EndEffectorWrist.java
+++ src/main/java/frc/robot/subsystems/EndEffectorWrist.java
@@ -5,12 +5,10 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.MotionMagicDutyCycle;
 import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.ENDEFFECTOR;
 import frc.robot.constants.V2CAN;
@@ -23,19 +21,16 @@ public class EndEffectorWrist extends SubsystemBase {
   private final NeutralModeValue m_neutralMode = NeutralModeValue.Brake;
 
   private final MotionMagicTorqueCurrentFOC m_request = new MotionMagicTorqueCurrentFOC(0);
-  // private doulbe m_desiredRotations = 
-  /** Creates a nepw EndEffectorWrist. */
 
+  // private doulbe m_desiredRotations =
+  /** Creates a nepw EndEffectorWrist. */
   public EndEffectorWrist() {
     TalonFXConfiguration configuration = new TalonFXConfiguration();
-      configuration.Slot0.kP = ENDEFFECTOR.kP;
-      configuration.Slot0.kI = ENDEFFECTOR.kI;
-      configuration.Slot0.kD = ENDEFFECTOR.kD;
-      configuration.MotorOutput.NeutralMode = m_neutralMode;
-      configuration.Feedback.RotorToSensorRatio = ENDEFFECTOR.endEffectorGearRatio;
-    
-
-
+    configuration.Slot0.kP = ENDEFFECTOR.kP;
+    configuration.Slot0.kI = ENDEFFECTOR.kI;
+    configuration.Slot0.kD = ENDEFFECTOR.kD;
+    configuration.MotorOutput.NeutralMode = m_neutralMode;
+    configuration.Feedback.RotorToSensorRatio = ENDEFFECTOR.endEffectorGearRatio;
   }
 
   @Override
