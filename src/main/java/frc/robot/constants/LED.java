97224fe816197c737b57db9d1cf8c8e3a846b1af
DeNeefe <puzzlekitticatty@gmail.com>
2025-02-15 12:07:18 -0800

    I just copy and pasted code from last year
    copied to LED constant and LEDSubsystem

--- src/main/java/frc/robot/constants/LED.java
+++ src/main/java/frc/robot/constants/LED.java
@@ -4,5 +4,52 @@
 
 package frc.robot.constants;
 
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+
 /** Add your docs here. */
-public class LED {}
+public class LED {
+    public static final int LEDcount = 34; // change this amount
+
+  public static final double LEDstripLength = Units.inchesToMeters(7); // meters
+
+  /** Different LED animation types */
+  public enum ANIMATION_TYPE {
+    ColorFlow,
+    ColorFlowLong,
+    Fire,
+    Larson,
+    Rainbow,
+    RgbFade,
+    SingleFade,
+    Strobe,
+    Twinkle,
+    TwinkleOff,
+    Solid
+  }
+
+  // These color are channels passed in the setPattern() method in the LED subsystem
+  public static final Color8Bit red = new Color8Bit(255, 0, 0);
+  public static final Color8Bit green = new Color8Bit(0, 255, 0);
+  public static final Color8Bit blue = new Color8Bit(0, 0, 255);
+  public static final Color8Bit yellow = new Color8Bit(150, 120, 0);
+  public static final Color8Bit purple = new Color8Bit(128, 0, 128);
+  public static final Color8Bit orange = new Color8Bit(247, 116, 40);
+  public static final Color8Bit pink = new Color8Bit(190, 30, 35);
+  public static final Color8Bit white = new Color8Bit(125, 125, 125);
+  public static final Color8Bit turquoise = new Color8Bit(24, 94, 89);
+
+  public enum SUBSYSTEM_STATES { //TODO: change these values
+    DISABLED,
+    SETUP_LOCALIZED,
+    SETUP_READY,
+    ENABLED,
+    INTAKED,
+    INTAKING,
+    REVED,
+    UNREVED,
+    ARMSCORING,
+    CLIMBING,
+    LOCKED
+  }
+}
--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -4,17 +4,141 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.led.Animation;
 import com.ctre.phoenix.led.CANdle;
-
+import com.ctre.phoenix.led.CANdle.LEDStripType;
+import com.ctre.phoenix.led.CANdle.VBatOutputMode;
+import com.ctre.phoenix.led.CANdleConfiguration;
+import com.ctre.phoenix.led.CANdleStatusFrame;
+import com.ctre.phoenix.led.ColorFlowAnimation;
+import com.ctre.phoenix.led.ColorFlowAnimation.Direction;
+import com.ctre.phoenix.led.FireAnimation;
+import com.ctre.phoenix.led.LarsonAnimation;
+import com.ctre.phoenix.led.LarsonAnimation.BounceMode;
+import com.ctre.phoenix.led.RainbowAnimation;
+import com.ctre.phoenix.led.RgbFadeAnimation;
+import com.ctre.phoenix.led.SingleFadeAnimation;
+import com.ctre.phoenix.led.StrobeAnimation;
+import com.ctre.phoenix.led.TwinkleAnimation;
+import com.ctre.phoenix.led.TwinkleAnimation.TwinklePercent;
+import com.ctre.phoenix.led.TwinkleOffAnimation;
+import com.ctre.phoenix.led.TwinkleOffAnimation.TwinkleOffPercent;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.CAN;
+import frc.robot.constants.LED;
+import frc.robot.constants.LED.*;
+import frc.robot.constants.ROBOT;
+
 
 public class LEDSubsystem extends SubsystemBase {
   /** Creates a new LEDSubsystem. */
   private final CANdle m_candle = new CANdle(CAN.CANdle);
 
+  private Color8Bit m_color = new Color8Bit();
+  private int m_red = 0;
+  private int m_green = 0; // setting all LED colors to none: there is no color when robot activates
+  private int m_blue = 0;
+  private int m_white = 0; // we have a seperate bulb in our LED strips for white
+  private double m_brightness = 0;
+  private double m_speed = 0;
+  // private SUBSYSTEM_STATES currentRobotState = SUBSYSTEM_STATES.DISABLED; //TODO: I'll fix this later once I have the GetSubsystemStates class done
+  private boolean setSolid; // this stops the animation
+  private Animation m_toAnimate = null;
 
-  public LEDSubsystem() {}
+  public LEDSubsystem() {
+    m_candle.configFactoryDefault();
+    // sets up LED strip
+    CANdleConfiguration configAll = new CANdleConfiguration();
+     configAll.statusLedOffWhenActive = true; // sets lights of when the LEDs are activated
+    configAll.disableWhenLOS = false; // disables LEDs when there is no signal for control
+    configAll.stripType = LEDStripType.GRB;
+    configAll.brightnessScalar =
+        0.5; // 1 is highest we can go we don't want to blind everyone at the event
+    configAll.vBatOutputMode = VBatOutputMode.Modulated; // Modulate
+    m_candle.configAllSettings(configAll, 100);
+    m_candle.setStatusFramePeriod(CANdleStatusFrame.CANdleStatusFrame_Status_1_General, 255);
+    m_candle.setStatusFramePeriod(CANdleStatusFrame.CANdleStatusFrame_Status_2_Startup, 255);
+    m_candle.setStatusFramePeriod(
+        CANdleStatusFrame.CANdleStatusFrame_Status_3_FirmwareApiStatus, 255);
+    m_candle.setStatusFramePeriod(CANdleStatusFrame.CANdleStatusFrame_Status_4_ControlTelem, 255);
+    m_candle.setStatusFramePeriod(
+        CANdleStatusFrame.CANdleStatusFrame_Status_5_PixelPulseTrain, 255);
+    m_candle.setStatusFramePeriod(CANdleStatusFrame.CANdleStatusFrame_Status_6_BottomPixels, 255);
+    m_candle.setStatusFramePeriod(CANdleStatusFrame.CANdleStatusFrame_Status_7_TopPixels, 255);
+  }
+
+  // will create LED patterns
+  public void setPattern(Color8Bit color, int white, double speed, ANIMATION_TYPE toChange) {
+    m_color = color;
+    m_red = color.red;
+    m_green = color.green;
+    m_blue = color.blue;
+    m_white = white;
+    m_speed = speed;
+    switch (toChange) {
+      case ColorFlow: // stripe of color flowing through the LED strip
+        m_toAnimate =
+            new ColorFlowAnimation(
+                m_red, m_green, m_blue, m_white, m_speed, LED.LEDcount, Direction.Forward);
+        break;
+      case ColorFlowLong: // stripe of color flowing through the LED strip
+        m_toAnimate =
+            new ColorFlowAnimation(
+                m_red, m_green, m_blue, m_white, m_speed, LED.LEDcount + 15, Direction.Forward);
+        break;
+
+      case Fire: // red and orange LEDs flaming up and down the LED strip
+        m_brightness = 0.5;
+        m_speed = 0.7;
+        m_toAnimate = new FireAnimation(m_brightness, m_speed, LED.LEDcount, 0.7, 0.5);
+        break;
+      case Larson: // a line bouncing back and forth with its width determined by size
+        m_toAnimate =
+            new LarsonAnimation(
+                m_red, m_green, m_blue, m_white, m_speed, LED.LEDcount, BounceMode.Front, 7);
+        break;
+      case Rainbow: // neon cat type beat
+        m_brightness = 1;
+        m_toAnimate = new RainbowAnimation(m_brightness, m_speed, LED.LEDcount);
+        break;
+      case RgbFade: // cycling between red, greed, and blue
+        m_brightness = 1;
+        m_toAnimate = new RgbFadeAnimation(m_brightness, m_speed, LED.LEDcount);
+        break;
+      case SingleFade: // slowly turn all LEDs from solid color to off
+        m_toAnimate =
+            new SingleFadeAnimation(m_red, m_green, m_blue, m_white, m_speed, LED.LEDcount);
+        break;
+      case Strobe: // switching between solid color and full off at high speed
+        m_toAnimate = new StrobeAnimation(m_red, m_green, m_blue, m_white, m_speed, LED.LEDcount);
+        break;
+      case Twinkle: // random LEDs turning on and off with certain color
+        m_toAnimate =
+            new TwinkleAnimation(
+                m_red, m_green, m_blue, m_white, m_speed, LED.LEDcount, TwinklePercent.Percent6);
+        break;
+      case TwinkleOff: // twinkle in reverse
+        m_toAnimate =
+            new TwinkleOffAnimation(
+                m_red,
+                m_green,
+                m_blue,
+                m_white,
+                m_speed,
+                LED.LEDcount,
+                TwinkleOffPercent.Percent100);
+        break;
+      case Solid:
+        m_toAnimate = null;
+        break;
+      default:
+        //        System.out.println("Incorrect animation type provided to changeAnimation()
+        // method");
+        break;
+    }
+  }
 
   @Override
   public void periodic() {
