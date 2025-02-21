646b3c7859d0f7db449434f321486b8a7fa2f37a
Ronan-B <rdavidbouza@gmail.com>
2025-02-20 18:35:18 -0800

    fixed gear ratio

--- src/main/java/frc/robot/generated/V2Constants.java
+++ src/main/java/frc/robot/generated/V2Constants.java
@@ -88,11 +88,10 @@ public class V2Constants {
 
   // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
   // This may need to be tuned to your individual robot
-  private static final double kCoupleRatio = 3.125;
+  private static final double kCoupleRatio = 3.5714285714285716;
 
-  private static final double kDriveGearRatio = 5.357142857142857;
-  private static final double kSteerGearRatio = 12.8;
-  /* ---------------------------------- */
+  private static final double kDriveGearRatio = 6.122448979591837;
+  private static final double kSteerGearRatio = 21.428571428571427; 
   private static final Distance kWheelRadius = Inches.of(2);
 
   private static final boolean kInvertLeftSide = false;
