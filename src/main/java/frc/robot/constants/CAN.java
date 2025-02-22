7847195da72e951ecb9534354cd83abb111431a8
DeNeefe <puzzlekitticatty@gmail.com>
2025-02-21 19:44:57 -0800

    i hate everyone

---
+++ src/main/java/frc/robot/constants/CAN.java
@@ -0,0 +1,48 @@
+package frc.robot.constants;
+
+public class CAN {
+  public static final String rioCanbus = "rio";
+  public static String driveBaseCanbus = "drivebase";
+
+  public static final int CANdle = 8; // LED
+  public static final int pigeon = 9;
+
+  public static final int frontLeftCanCoder = 10;
+  public static final int frontRightCanCoder = 11;
+  public static final int backLeftCanCoder = 12;
+  public static final int backRightCanCoder = 13;
+
+  public static final int frontLeftDriveMotor = 20;
+  public static final int frontLeftTurnMotor = 21;
+  public static final int frontRightDriveMotor = 22;
+  public static final int frontRightTurnMotor = 23;
+  public static final int backLeftDriveMotor = 24;
+  public static final int backLeftTurnMotor = 25;
+  public static final int backRightDriveMotor = 26;
+  public static final int backRightTurnMotor = 27;
+
+  public static final int hopperIntakeMotor =
+      30; // 31 is not here because everyone sucks and i hate everyone. I want to cry. it took me two hours to fix my VScode. 
+
+  public static final int endEffectorPivotMotor = 32;
+  public static final int endEffectorOuttakeMotor = 35;
+  public static final int endEffectorPivotCanCoder = 36;
+
+  public static final int elevatorMotor1 = 33;
+  public static final int elevatorMotor2 = 34;
+  public static final int endEffectorMotor = 35;
+
+  public static final int outtakePivotCANcoder = 36;
+
+  // L1/Ground
+  public static final int groundRollerMotor = 40;
+  public static final int groundPivotMotor = 41;
+
+  public static final int groundPivotCANcoder = 42;
+
+  // Climber
+  public static final int cageMotor = 50;
+  public static final int climberMotor = 51;
+
+  public static final int climberCANcoder = 52;
+}
