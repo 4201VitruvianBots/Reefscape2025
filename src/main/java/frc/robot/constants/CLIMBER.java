b61f0299b070881f9068a3e4d404e555a0630efb
Charlotte P <charlottepeeler@gmail.com>
2025-02-23 14:55:36 -0800

    deleted climber intake

--- src/main/java/frc/robot/constants/CLIMBER.java
+++ src/main/java/frc/robot/constants/CLIMBER.java
@@ -34,11 +34,5 @@ public class CLIMBER {
     }
   }
 
-  public class INTAKE {
-    public static final double kP = 0.0;
-    public static final double kI = 0.0;
-    public static final double kD = 0.0;
-    public static final double gearRatio = 1 / 32;
-    public static final DCMotor gearbox = DCMotor.getKrakenX60(1);
-  }
+
 }
