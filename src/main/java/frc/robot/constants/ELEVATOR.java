60b24b306d8934ffb937b9ac25d51dcaca933f0e
Nathan S <nathanael7639@gmail.com>
2025-11-02 10:34:16 -0800

    Add lowered elevator

--- src/main/java/frc/robot/constants/ELEVATOR.java
+++ src/main/java/frc/robot/constants/ELEVATOR.java
@@ -57,8 +57,8 @@ public class ELEVATOR {
     INTAKE_HOPPER(Inches.of(0.4)), // used to be 3.543
     LEVEL_2(Inches.of(13.0)),
     LEVEL_3(Inches.of(27.0)),
-    LEVEL_4(Inches.of(57.0)),
-    LEVEL_4_TELEOP_SCORE(Inches.of(56.0)),
+    LEVEL_4(Inches.of(57.0)), // Auto L4 elevator height
+    LEVEL_4_TELEOP_SCORE(Inches.of(55.0)), //L4 autoscore and regular L4 in teleop
     NET(Inches.of(57.5));
 
     private final Distance setpoint;
